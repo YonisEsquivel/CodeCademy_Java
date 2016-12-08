64c4205cb5b5ca217c2e4598efbab99ac7bb616b
Yonis Esquivel Gamarra <info.contactoprofesional@gmail.com>
2016-12-08 14:03:23 -0500

    Variables

--- src/yonis/YourName.java
+++ src/yonis/YourName.java
@@ -3,5 +3,15 @@ package yonis;
 public class YourName {
 	public static void main (String[] args){
 		System.out.println("Yonis Esquivel");
+		System.out.println(1987);
+		System.out.println(true);
+		System.out.println('Y');
+		
+		//VARIABLES
+		int myNumber = 47;
+		boolean isFun = true;
+		char movieRating = 'A';
+		
+		
 	}
 }
